2b5f646006c5869c2a8a20b44745075c91bbd44f
redjun89 <jjune1816@gmail.com>
2023-05-22 20:47:48 +0900

    board model 정의 및 타입 정의

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
+
+
+@Injectable()
+export class BoardsService {
+    private boards: Board[] = [];
+
+    getAllBoards(): Board[] {
+        return this.boards;
+    }
+}
