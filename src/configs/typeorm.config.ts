f43b7407a76b328f93601b333bc9249e4171b8a7
redjun89 <jjune1816@gmail.com>
2023-05-23 22:58:07 +0900

    typeorm 옵션 에러 수정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,12 @@
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-export const typeORMConfig : TypeOrmModule = {
-    typr: 'postgres',
+export const typeORMConfig : TypeOrmModuleOptions = {
+    type: 'postgres',
     host: 'localhost',
     port: 5432,
     username: 'postgres',
     password: 'redjun89!@',
     database: 'board-app',
-    entities: [__dirname + '/../**/*.ertity.{js,ts}'],
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
 }
