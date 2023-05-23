ccc81f5b425cdf62ec473a55a325e163ce38712d
redjun89 <jjune1816@gmail.com>
2023-05-23 22:58:56 +0900

    사용하지 않는 import 삭제

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
+import { BoardRepository } from './board.repository'; // 추가
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([BoardRepository])
+    TypeOrmModule.forFeature([Board])
   ],
   controllers: [BoardsController],
-  providers: [BoardsService]
+  providers: [BoardsService, BoardRepository] // BoardRepository 추가
 })
-export class BoardsModule {}
+export class BoardsModule { }
