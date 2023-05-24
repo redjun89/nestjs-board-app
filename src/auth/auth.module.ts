23614edd76ab1226961e2aebf81561d79ca58cbe
redjun89 <jjune1816@gmail.com>
2023-05-24 10:12:24 +0900

    유저 회원가입, 로그인을 위한 기본 세팅

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserRepository } from './user.repository';
 
 @Module({
-  controllers: [AuthController]
+  imports: [
+    TypeOrmModule.forFeature([UserRepository])
+  ],
+  controllers: [AuthController],
+  providers: [AuthService]
 })
 export class AuthModule {}
