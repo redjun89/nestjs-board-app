9639997a7482895364d5ed61521d41122231034d
redjun89 <jjune1816@gmail.com>
2023-05-24 14:07:26 +0900

    로그인 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -11,5 +12,16 @@ export class AuthService {
 
     signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.signUp(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.userRepository.findOneBy({ username });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            return '로그인 성공'
+        } else {
+            throw new UnauthorizedException('로그인 실패');
+        }
     }
 }
