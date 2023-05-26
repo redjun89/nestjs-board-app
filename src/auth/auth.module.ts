ec4c72e9c7094bb3122c29009c20022740b5a889
redjun89 <jjune1816@gmail.com>
2023-05-26 10:28:04 +0900

    feature # 환경설정 및 적용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,14 +7,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'secretkey',
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions: {
-        expiresIn: 60 * 60,
+        expiresIn: jwtConfig.expiresIn,
       }
     }),
     TypeOrmModule.forFeature([User])
